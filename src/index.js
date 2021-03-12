bfd018eefbcd23f1d87f83830f55c830009a0d45
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-03-12 18:01:39 +0200

    init: start Array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    } else {
+        var reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return array.reduce(reducer) / array.length;
+    }
+};
